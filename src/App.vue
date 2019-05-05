7e4cc7cd26195d36dc1ad76047169227438be707
Anles <122177638@qq.com>
2019-05-05 11:06:49 +0800

    新增主题颜色

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav" class="color">
+    <div
+      id="nav"
+      class="color"
+    >
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <transition :name="transitionName">
-      <keep-alive :max="10" :include="keepAlive">
-        <navigation> <router-view></router-view> </navigation>
+      <keep-alive
+        :max="10"
+        :include="keepAlive"
+      >
+        <navigation>
+          <router-view></router-view>
+        </navigation>
       </keep-alive>
     </transition>
   </div>
@@ -22,14 +30,11 @@ export default {
     };
   },
   created() {
-    // 遍历路由设置keepAlive
-    router.options.routes.map(item => {
-      if (item.meta && item.meta.keepAlive) {
-        this.keepAlive.push(item.name);
-      }
-    });
+    // 递归路由设置KeepAlive  ***** 注意路由name必须和组件内的name一致 *****
+    this.setRouteKeepAlive(router.options.routes);
+
     // 记录路由,动态给定动画.详细功能https://www.npmjs.com/package/vue-navigation
-    this.$navigation.on("forward", (to, from) => {
+    this.$navigation.on("forward", to => {
       this.transitionName = to.route.meta.isTransition ? "slide-left" : "";
     });
     this.$navigation.on("back", (to, from) => {
@@ -51,6 +56,25 @@ export default {
       // 设置document.title
       if (to.meta.title) document.title = to.meta.title;
     }
+  },
+  methods: {
+    /**
+     * @description: 递归路由设置KeepAlive
+     * @param {Array}  routes 路由数据
+     * @return: undefined
+     * @Date: 2019-05-04 14:57:18
+     */
+    setRouteKeepAlive(routes) {
+      routes.map(item => {
+        if (item.children) {
+          this.setRouteKeepAlive(item.children);
+        } else {
+          if (item.meta && item.meta.keepAlive) {
+            this.keepAlive.push(item.name);
+          }
+        }
+      });
+    }
   }
 };
 </script>
@@ -65,23 +89,25 @@ export default {
     "Heiti SC", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
   width: 100%;
   height: 100%;
-  text-align: center;
   overflow: hidden;
   background-color: #f5f5f5;
 }
-// 切换动画
 .slide-right-enter-active,
 .slide-right-leave-active,
 .slide-left-enter-active,
 .slide-left-leave-active {
   will-change: transform;
   transition: all 450ms;
-  position: fixed;
+  position: absolute;
   top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
+  backface-visibility: hidden;
+  perspective: 800;
 }
 .slide-right-enter {
-  opacity: 0;
+  opacity: 1;
   transform: translate3d(-100%, 0, 0);
 }
 .slide-right-leave-active {
@@ -89,7 +115,7 @@ export default {
   transform: translate3d(100%, 0, 0);
 }
 .slide-left-enter {
-  opacity: 0;
+  opacity: 1;
   transform: translate3d(100%, 0, 0);
 }
 .slide-left-leave-active {
--- src/api/axios.js
+++ src/api/axios.js
@@ -2,23 +2,46 @@ import Vue from "vue";
 import axios from "axios";
 import envconfig from "./envconfig";
 import qs from "qs";
+// import store from "@/store/store";
+import router from "@/router/router";
 
 // 发起请求前
 let loadingInstance = null;
 
+// 状态码错误信息
+const codeMessage = {
+  200: "服务器成功返回请求的数据。",
+  201: "新建或修改数据成功。",
+  202: "一个请求已经进入后台排队（异步任务）。",
+  204: "删除数据成功。",
+  400: "发出的请求有错误，服务器没有进行新建或修改数据的操作。",
+  401: "用户没有权限（令牌、用户名、密码错误）。",
+  403: "用户得到授权，但是访问是被禁止的。",
+  404: "发出的请求针对的是不存在的记录，服务器没有进行操作。",
+  406: "请求的格式不可得。",
+  410: "请求的资源被永久删除，且不会再得到的。",
+  422: "当创建一个对象时，发生一个验证错误。",
+  500: "服务器发生错误，请检查服务器。",
+  502: "网关错误。",
+  503: "服务不可用，服务器暂时过载或维护。",
+  504: "网关超时。"
+};
+
+// 创建拦截器
 let axiosInstance = axios.create();
+// 发起请求前
 axiosInstance.interceptors.request.use(
   config => {
     // 默认开启loading
     if (!config.LOADINGHIDE) {
-      loadingInstance = Vue.prototype.$loading({
-        lock: true,
-        text: "Loading",
-        spinner: "el-icon-loading",
-        background: "rgba(0, 0, 0, 0.7)"
-      });
+      // loadingInstance = Vue.prototype.$loading({
+      //   lock: true,
+      //   text: "Loading",
+      //   spinner: "el-icon-loading",
+      //   background: "rgba(0, 0, 0, 0.7)"
+      // });
     }
-    if (config.method.toUpperCase() === "POST") {
+    if (config.method.toUpperCase() !== "GET") {
       config.data = qs.stringify(config.data);
     }
     return config;
@@ -31,6 +54,7 @@ axiosInstance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
 // 发起请求后
 axiosInstance.interceptors.response.use(
   res => {
@@ -50,19 +74,32 @@ axiosInstance.interceptors.response.use(
     return res;
   },
   error => {
-    console.log("好多人在访问呀，请重新试试");
     // loading close...
     loadingInstance && loadingInstance.close();
     if (error) {
-      let errortime = null;
-      clearTimeout(errortime);
-      errortime = setTimeout(() => {
-        Vue.prototype.$message({
-          message: "网络错误",
-          type: "error"
-        });
-        clearTimeout(errortime);
-      }, 0);
+      // 请求配置发生的错误
+      if (!error.response) {
+        return console.log("Error", error.message);
+      }
+
+      // 获取状态码
+      const status = error.response.status;
+      const errortext = codeMessage[status] || error.response.statusText;
+
+      // 提示错误信息
+      Vue.prototype.$message({
+        message: errortext,
+        type: "error"
+      });
+
+      // 错误状态处理
+      if (status === 401) {
+        router.push("/login");
+      } else if (status === 403) {
+        router.push("/login");
+      } else if (status >= 404 && status < 422) {
+        router.push("/404");
+      }
     }
     return Promise.reject(error);
   }
@@ -78,14 +115,22 @@ export default class Axios {
           url,
           baseURL: envconfig.baseURL,
           timeout: 10000,
-          headers: null
+          headers: {}
           // withCredentials: true, //是否携带cookies发起请求
         },
         config
       );
-      method.toUpperCase() === "POST"
+      // 添加token
+      // _option.headers = {
+      //   ..._option.headers,
+      //   authorization: "Bearer " + store.getters.token
+      // };
+      // 处理get、post传参问题
+      method.toUpperCase() !== "GET"
         ? (_option.data = params)
         : (_option.params = params);
+
+      // 请求成功后服务器返回二次处理
       axiosInstance.request(_option).then(
         res => {
           resolve(res.data);
